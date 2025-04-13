02a3a7c21dde117ef3c38517c6ba6a8a538e79cf
AbubakrBardien <abubakrbardien@gmail.com>
2025-04-14 00:21:47 +0200

    Minor Changes

--- lua/plugin_configs/statusline/custom_theme.lua
+++ lua/plugin_configs/statusline/custom_theme.lua
@@ -9,40 +9,40 @@ local colors = {
 	red1 = "#f07178",
 	red2 = "#ff5370",
 	orange = "#D8B576",
-	gray1 = "#314549",
-	gray2 = "#24262b",
-	gray3 = "#3a3f42",
+	grey1 = "#314549",
+	grey2 = "#24262b",
+	grey3 = "#3a3f42",
 }
 
 return {
 	normal = {
 		a = { fg = colors.bg, bg = colors.blue, gui = "bold" },
-		b = { fg = colors.blue, bg = colors.gray3, gui = "bold" },
-		c = { fg = colors.fg, bg = colors.gray2 },
+		b = { fg = colors.blue, bg = colors.grey3, gui = "bold" },
+		c = { fg = colors.fg, bg = colors.grey2 },
 	},
 	insert = {
 		a = { fg = colors.bg, bg = colors.green, gui = "bold" },
-		b = { fg = colors.green, bg = colors.gray3, gui = "bold" },
+		b = { fg = colors.green, bg = colors.grey3, gui = "bold" },
 	},
 	visual = {
 		a = { fg = colors.bg, bg = colors.purple, gui = "bold" },
-		b = { fg = colors.purple, bg = colors.gray3, gui = "bold" },
+		b = { fg = colors.purple, bg = colors.grey3, gui = "bold" },
 	},
 	replace = {
 		a = { fg = colors.bg, bg = colors.red1, gui = "bold" },
-		b = { fg = colors.red1, bg = colors.gray3, gui = "bold" },
+		b = { fg = colors.red1, bg = colors.grey3, gui = "bold" },
 	},
 	command = {
 		a = { fg = colors.bg, bg = colors.orange, gui = "bold" },
-		b = { fg = colors.orange, bg = colors.gray3, gui = "bold" },
+		b = { fg = colors.orange, bg = colors.grey3, gui = "bold" },
 	},
 	terminal = {
 		a = { fg = colors.bg, bg = colors.green, gui = "bold" },
-		b = { fg = colors.orange, bg = colors.gray3, gui = "bold" },
+		b = { fg = colors.orange, bg = colors.grey3, gui = "bold" },
 	},
 	inactive = {
 		a = { fg = colors.fg, bg = colors.bg, gui = "bold" },
 		b = { fg = colors.fg, bg = colors.bg },
-		c = { fg = colors.fg, bg = colors.gray2 },
+		c = { fg = colors.fg, bg = colors.grey2 },
 	},
 }
