390b40020e5ef54f594194ebb2d1915782a61e9c
AbubakrBardien <abubakrbardien@gmail.com>
2025-05-03 22:06:13 +0200

    Tweaked the background-color

--- lua/plugin_configs/onedarkpro.lua
+++ lua/plugin_configs/onedarkpro.lua
@@ -4,7 +4,8 @@ return {
 	priority = 1000, -- Ensure it loads first
 	config = function()
 		require("onedarkpro").setup {
-			colors = { onedark = { bg = "#1d1f24" } },
+			-- colors = { onedark = { bg = "#1d1f24" } },
+			colors = { onedark = { bg = "#191c1f" } },
 		}
 	end,
 }
