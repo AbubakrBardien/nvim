8b185ee2d3c837e7288fd77b74e07fa64749d0f7
AbubakrBardien <abubakrbardien@gmail.com>
2025-05-30 21:17:43 +0200

    Added glyphs for diagnostic signs

--- lua/plugin_configs/mason_nvim/linters.lua
+++ lua/plugin_configs/mason_nvim/linters.lua
@@ -1,9 +1,24 @@
 return {
-	"mfussenegger/nvim-lint",
-	event = { "BufReadPre", "BufNewFile", "BufWritePost" },
-	config = function()
-		require("lint").linters_by_ft = {
-			sh = { "shellcheck" }, -- Needed for "bash-langauge-server"
-		}
-	end,
+	{
+		"mfussenegger/nvim-lint",
+		event = { "BufReadPre", "BufNewFile", "BufWritePost" },
+		config = function()
+			require("lint").linters_by_ft = {
+				sh = { "shellcheck" }, -- Needed for "bash-langauge-server"
+			}
+		end,
+	},
+
+	vim.diagnostic.config {
+		virtual_text = false,
+		update_in_insert = true,
+		signs = {
+			text = {
+				[vim.diagnostic.severity.ERROR] = "",
+				[vim.diagnostic.severity.WARN] = "",
+				[vim.diagnostic.severity.INFO] = "",
+				[vim.diagnostic.severity.HINT] = "",
+			},
+		},
+	},
 }
